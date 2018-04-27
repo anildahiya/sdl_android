838252f3ab7e6df2a02fc9ae08a326476fb47b58
Anil Dahiya <anil_dahiya@infosys.com>
2018-04-27 16:54:24 +0530

    Changes for additions to RequestType enum and additions to SystemRequest Rpc.

--- sdl_android/src/main/java/com/smartdevicelink/proxy/rpc/SystemRequest.java
+++ sdl_android/src/main/java/com/smartdevicelink/proxy/rpc/SystemRequest.java
@@ -47,6 +47,8 @@ public class SystemRequest extends RPCRequest {
 	public static final String KEY_FILE_NAME = "fileName";
 	public static final String KEY_REQUEST_TYPE = "requestType";
 	public static final String KEY_DATA = "data";
+    public static final String KEY_REQUEST_SUB_TYPE = "requestSubType";
+
 	/**
 	 * Constructs a new SystemRequest object
 	 */ 
@@ -86,5 +88,13 @@ public class SystemRequest extends RPCRequest {
 
     public void setRequestType(RequestType requestType) {
         setParameters(KEY_REQUEST_TYPE, requestType);
+    }
+
+    public String getRequestSubType() {
+        return getString(KEY_REQUEST_SUB_TYPE);
+    }
+
+    public void setRequestSubType(String requestSubType) {
+        setParameters(KEY_REQUEST_SUB_TYPE, requestSubType);
     }
 }
